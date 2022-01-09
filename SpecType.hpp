0e891ffbedfa4f059a6ca252976282a6dc17939d
Robert Booth <boothr16@gmail.com>
2022-01-09 14:52:50 -0500

    Added header guards to SpecType.hpp

--- SpecType.hpp
+++ SpecType.hpp
@@ -1,9 +1,12 @@
+#ifndef SPECTYPE_HPP
+#define SPECTYPE_HPP
+
 #include <string>
 #include <set>
 
 class SpecType {
     private:
-        std::string name;
+        std::string type;
     public:
         SpecType(std::string &spec);
         bool isValid();
@@ -13,14 +16,16 @@ SpecType::SpecType(std::string &spec) {
     for (int i = 0; i < spec.length(); i++) {
         spec[i] = toupper(spec[i]);
     }
-    name = spec;
+    type = spec;
 }
 
 bool SpecType::isValid() {
-    if (name.length() < 2)
+    if (type.length() < 2)
         return false;
     const std::set<std::string> SPEC_TYPES = {"UV", "MS"};
-    if (SPEC_TYPES.find(name) == SPEC_TYPES.end())
+    if (SPEC_TYPES.find(type) == SPEC_TYPES.end())
         return false;
     return true;
 }
+
+#endif
